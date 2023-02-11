68398d1544a1508542869f4897b7ad1162874154
saurabh-ku <krsaurabh.dev@gmail.com>
2023-02-11 19:41:26 +0530

    Json response from gpt

--- backend/app/api.py
+++ backend/app/api.py
@@ -3,9 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 import openai
 import csv
 from io import StringIO
+import json
+
 
 openai.organization = "org-0UjCDso2b7GIHvHREYwFMQLF"
-openai.api_key = "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"
+openai.api_key = "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"
 
 app = FastAPI()
 
@@ -25,35 +27,16 @@ app.add_middleware(
 
 @app.get("/pick", tags=["pick"])
 async def get_todos(data: dict) -> dict:
-    print(', '.join(data['enemy_picks']))
-    print(', '.join(data['my_picks']))
     hero_picks = openai.Completion.create(
         model="text-davinci-003",
         prompt='''
         Oponent team : {}
         My team : {}
-        Suggest 5 hero to pick along with rating for picking in the following csv format : Hero Name, Rating from 0 to 10, Reason, Which oponent hero name it counters
+        Suggest 5 hero to pick along with rating for picking in the following json format 
+        {{"hero_suggestions" : [{{'hero_name' 'rating from 0 to 10' 'reason for the counter pick' 'counter for hero name'}}]}}
         '''.format(', '.join(data['enemy_picks']), ', '.join(data['my_picks']) ),
         max_tokens=500,
         temperature=0.5
     )['choices'][0]['text']
 
-
-    print(hero_picks)
-
-    csv_file = StringIO(hero_picks)
-    reader = csv.reader(csv_file)
-
-    data = []
-    for row in reader:
-        if row == []:
-            continue
-
-        data.append({
-            'name': row[0].strip(),
-            'rating': row[1].strip(),
-            'reason': row[2].strip(),
-            'counters' : row[3].strip()
-        })
-    
-    return data
+    return json.loads(hero_picks)
