309ff09d4530e60c5df6ace14ecc7d9dcf6972a5
vsramanujan <rama.psbb@gmail.com>
2023-02-11 18:35:24 +0530

    fix: general frontend cleanup

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,22 +1,7 @@
 import React from "react";
-import { Heading, Flex, Divider } from "@chakra-ui/react";
 
 const Header = () => {
-  return (
-      <Flex
-          as="nav"
-          align="center"
-          justify="space-between"
-          wrap="wrap"
-          padding="0.5rem"
-          bg="gray.400"
-      >
-        <Flex align="center" mr={5}>
-          <Heading as="h1" size="sm">Todos</Heading>
-          <Divider />
-        </Flex>
-      </Flex>
-  );
+  return <div>Header</div>;
 };
 
 export default Header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { render } from 'react-dom';
-import { ChakraProvider } from "@chakra-ui/core";
+import { render } from "react-dom";
 
 import Header from "./Components/Header";
 import Todos from "./Components/Todos";
 
 function App() {
   return (
-    <ChakraProvider>
+    <>
       <Header />
       <Todos />
-    </ChakraProvider>
-  )
+    </>
+  );
 }
 
-const rootElement = document.getElementById("root")
-render(<App />, rootElement)
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
